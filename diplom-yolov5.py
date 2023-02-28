8507ad26fa41874a286f6b4e6875d999b1afa712
lnjanoyan <100421669+lnjanoyan@users.noreply.github.com>
2023-02-28 17:15:20 +0400

    Add files via upload

---
+++ diplom-yolov5.py
@@ -0,0 +1,77 @@
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNet(r'C:\Users\User\Downloads\yolov5s.onnx')
+
+
+def format_yolov5(source):
+
+    # put the image in square big enough
+    col, row, _ = source.shape
+    _max = max(col, row)
+    resized = np.zeros((_max, _max, 3), np.uint8)
+    resized[0:col, 0:row] = source
+    
+    # resize to 640x640, normalize to [0,1[ and swap Red and Blue channels
+    result = cv2.dnn.blobFromImage(resized, 1/255.0, (640, 640), swapRB=True)
+    
+    return result
+
+
+predictions = net.forward()
+output = predictions[0]
+
+def unwrap_detection(input_image, output_data):
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    rows = output_data.shape[0]
+
+    image_width, image_height, _ = input_image.shape
+
+    x_factor = image_width / 640
+    y_factor =  image_height / 640
+
+    for r in range(rows):
+        row = output_data[r]
+        confidence = row[4]
+        if confidence >= 0.4:
+
+            classes_scores = row[5:]
+            _, _, _, max_indx = cv2.minMaxLoc(classes_scores)
+            class_id = max_indx[1]
+            if (classes_scores[class_id] > .25):
+
+                confidences.append(confidence)
+
+                class_ids.append(class_id)
+
+                x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item() 
+                left = int((x - 0.5 * w) * x_factor)
+                top = int((y - 0.5 * h) * y_factor)
+                width = int(w * x_factor)
+                height = int(h * y_factor)
+                box = np.array([left, top, width, height])
+                boxes.append(box)
+
+    return class_ids, confidences, boxes
+
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45) 
+
+result_class_ids = []
+result_confidences = []
+result_boxes = []
+
+for i in indexes:
+    result_confidences.append(confidences[i])
+    result_class_ids.append(class_ids[i])
+    result_boxes.append(boxes[i])
+
+
+
+# load the model as usual
+net = cv2.dnn.readNet('yolov5s.onnx')
+
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
